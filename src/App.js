0c8db6dce3da49dd7a2058b8d7ed08fe204024ce
Cole <deoncole77@gmail.com>
2022-06-09 22:55:38 -0400

    add tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-// import './App.css';
-import About from './components/About';
 import Nav from './components/Nav';
+import About from './components/About';
 import Gallery from './components/Gallery';
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
+  ]); 
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
@@ -25,14 +24,11 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
 }
 
 export default App;
-
